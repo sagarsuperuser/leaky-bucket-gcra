159eb972c077d2f57b2e72a84ea66aa26eaf4c85
sagar waidande <superuser@sagars-MacBook-Pro.local>
2025-11-27 13:41:16 +0530

    (chore): doc fix

--- doc.go
+++ doc.go
@@ -3,7 +3,6 @@
 // State for each key is stored in Redis via
 // a single Lua script, enabling low-latency distributed limits across processes.
 // The script uses redis.replicate_commands (Redis 3.2+)
-
 // Find optional demos under cmd/.
 //
 // Example:
